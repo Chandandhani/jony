2075a3f6655327dea9bdb96468d93c3bc534034d
janmenjaya755 <54701151+janmenjaya755@users.noreply.github.com>
2022-08-20 15:59:06 +0530

    jj

---
+++ batabillpunchsystem/src/main/java/com/bata/billpunch/BatabillpuchApplication.java
@@ -0,0 +1,26 @@
+package com.bata.billpunch;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class BatabillpuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatabillpuchApplication.class, args);
+	}
+	
+	@PostConstruct
+    public void init(){
+      // Setting Spring Boot SetTimeZone
+      TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
+	
+
+}
